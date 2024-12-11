9f5cfd226fa5d1e57fd9f2f23796fcfa6cc2a13a
Promesse10 <promesseirakoze10@gmail.com>
2024-12-11 10:07:00 +0200

    my first new portfolio

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,22 @@ export default {
     "./src/**/*.{vue,js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        blink: {
+          '0%, 100%': { opacity: 1 },
+          '50%': { opacity: 0 },
+        },
+        shimmer: {
+          '100%': { transform: 'translateX(100%)' }
+        }
+      },
+      animation: {
+        blink: 'blink 1s step-end infinite',
+        shimmer: 'shimmer 2s linear infinite'
+      }
+    }
+  
   },
   plugins: [],
 }
